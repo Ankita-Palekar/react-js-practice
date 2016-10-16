458066bb2e58a0f151ac6f03a2a02b17dd3c4109
Ankita-Palekar <453ankitapalekar@gmail.com>
2016-10-16 16:50:12 +0530

    -- using state to populate the data

--- app.jsx
+++ app.jsx
@@ -3,14 +3,41 @@ import React from 'react';
 
 // this is the main component where in everything will be wrapped inside
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-      	<Header/>  
-      	<Content/>
-      </div>
-    );
-  }
+	constructor(){
+ 		super();
+ 		this.state = {
+ 			data: [
+ 				{
+ 					"id": 1,
+ 					"name": "Ankita",
+ 					"age": "24"
+ 				},
+ 				{
+ 					"id": 2,
+ 					"name": "Apurva",
+ 					"age": "19"
+ 				},
+ 				{
+ 					"id": 3,
+ 					"name": "Aditi",
+ 					"age": "17"
+ 				}
+ 			]
+ 		};
+ 	}
+	render() {
+ 		return (
+    	<div>
+      	<Header/>
+       	<table>
+          <tbody>
+            {this.state.data.map((person, i) => <TableRow key = {i} data = {person} />)}
+          </tbody>
+       	</table>
+    	</div>
+ 		);
+	}
+	
 }
 
 class Header extends React.Component{
@@ -32,6 +59,19 @@ class Content extends React.Component{
 			</div>
 		);
 	}
+}
+
+
+class TableRow extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td>{this.props.data.id}</td>
+        <td>{this.props.data.name}</td>
+        <td>{this.props.data.age}</td>
+      </tr>
+    );
+  }
 }
 
 export default App;
