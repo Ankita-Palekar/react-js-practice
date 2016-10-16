6149e6d2a34e2d937452879477342ec7936679d2
Ankita-Palekar <453ankitapalekar@gmail.com>
2016-10-16 13:57:43 +0530

    -- using inline css for dom element

--- app.jsx
+++ app.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 
 class App extends React.Component {
-   render() {
+  render() {
    	var i = 1;
+  	var myStyle = {
+  		fontSize: 100,
+  		color: '#cccccc'
+  	}
 
-      return (
-         <div>
-            <h1>This is just a test</h1>
-            <p>You nuts</p>
-            <con>This is my attributes</con>
-            <p> {1==1? 'hi' : 'hello'}</p>
-         </div>
-      );
-   }
+    return (
+       <div>
+          <h1 style = {myStyle}>This is just a test</h1>
+          <p>You nuts</p>
+          <con>This is my attributes</con>
+          <p> {1==1? 'hi' : 'hello'}</p>
+        {/* this is my comment */}
+       </div>
+    );
+  }
 }
 
 export default App;
