a346fd638828e4b6bb615fe7e1f33da92e8dad6a
Ankita-Palekar <453ankitapalekar@gmail.com>
2016-10-17 00:27:32 +0530

    -- checking on the validations of props

--- app.jsx
+++ app.jsx
@@ -15,50 +15,47 @@ class App extends React.Component {
 	render() {
  		return (
     	<div>
-      	<Header headerProp={this.state.header} />
-      	<Content contentProp={this.state.content}/>
+    		<h3>Array: {this.props.propArray}</h3>
+    		<h3>Bool: {this.props.propBool}</h3>
+    		<h3>Func: {this.props.propFunc(3)}</h3>
+        <h3>Number: {this.props.propNumber}</h3>
+        <h3>String: {this.props.propString}</h3>
+        <h3>Object: {this.props.propObject.key1}</h3>
+        <h3>Object: {this.props.propObject.key2}</h3>
+        <h3>Object: {this.props.propObject.key3}</h3>
     	</div>
  		);
 	}
 	
 }
 
-	App.defaultProps = {
- 		headerProp: "Header from props...",
- 		contentProp:"Content from props..."
-	}
 
-class Header extends React.Component{
-	render(){
-		return(
-			<div>
-				<h1>{this.props.headerProp}</h1>
-			</div>
-		);
+App.propTypes = {
+	propArray: React.PropTypes.array.isRequired,
+	propBool: React.PropTypes.bool.isRequired,
+	propFunc: React.PropTypes.func,
+	propNumber: React.PropTypes.number,
+	propString: React.PropTypes.string,
+	propObject: React.PropTypes.object
+}
+
+App.defaultProps = {
+	propArray : [1, 2, 3, 4, 5, 6],
+	propBool : true,
+	propFunc : function(e){return e},
+	propNumber : 1,
+	propString : "String Value ...",
+	propObject: {
+		key1 : "key1",
+		key2 : "key2",
+		key3 : "key3",
+		key4 : "key4"
 	}
 }
 
-class Content extends React.Component{
-	render(){
-		return(
-			<div>
-				<h1>{this.props.contentProp}</h1>
-			</div>
-		);
-	}
-}
 
 
-class TableRow extends React.Component {
-  render() {
-    return (
-      <tr>
-        <td>{this.props.data.id}</td>
-        <td>{this.props.data.name}</td>
-        <td>{this.props.data.age}</td>
-      </tr>
-    );
-  }
-}
+
+
 
 export default App;
