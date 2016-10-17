11cee8b9a828aa31d94dde87c9d81024874d5061
Ankita-Palekar <453ankitapalekar@gmail.com>
2016-10-18 01:43:10 +0530

    -- props and state together

--- app.jsx
+++ app.jsx
@@ -10,21 +10,32 @@ class App extends React.Component {
     }
 
     this.updateState = this.updateState.bind(this);
-
   };
 
   updateState() {
-    this.setState({data: 'Data updated...'})
+    this.setState({data: 'Data updated from the child component...'})
   }
 
   render() {
     return (
-   		<div>
-      	<button onClick = {this.updateState}>CLICK</button>
-      	<h4>{this.state.data}</h4>
-   		</div>
+      <div>
+        <Content myDataProp = {this.state.data} updateStateProp = {this.updateState}></Content>
+      </div>
     );
   }
 }
+
+class Content extends React.Component {
+  render() {
+    return (
+      <div>
+        <button onClick = {this.props.updateStateProp}>CLICK</button>
+        <h3>{this.props.myDataProp}</h3>
+      </div>
+    );
+  }
+}
+
+ 
 
 export default App;
