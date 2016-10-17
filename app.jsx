c81cc0b9d75bab342684d7194b2646c796318131
Ankita-Palekar <453ankitapalekar@gmail.com>
2016-10-18 01:47:36 +0530

    -- refs - used to return the reference to the element

--- app.jsx
+++ app.jsx
@@ -6,36 +6,32 @@ class App extends React.Component {
     super(props);
 		
     this.state = {
-      data: 'Initial data...'
+       data: ''
     }
 
     this.updateState = this.updateState.bind(this);
+    this.clearInput = this.clearInput.bind(this);
   };
 
-  updateState() {
-    this.setState({data: 'Data updated from the child component...'})
+  updateState(e) {
+    this.setState({data: e.target.value});
+  }
+
+  clearInput() {
+    this.setState({data: ''});
+    ReactDOM.findDOMNode(this.refs.myInput).focus();
   }
 
   render() {
     return (
       <div>
-        <Content myDataProp = {this.state.data} updateStateProp = {this.updateState}></Content>
+        <input value = {this.state.data} onChange = {this.updateState} ref = "myInput"></input>
+        <button onClick = {this.clearInput}>CLEAR</button>
+        <h4>{this.state.data}</h4>
       </div>
     );
   }
 }
 
-class Content extends React.Component {
-  render() {
-    return (
-      <div>
-        <button onClick = {this.props.updateStateProp}>CLICK</button>
-        <h3>{this.props.myDataProp}</h3>
-      </div>
-    );
-  }
-}
-
- 
 
 export default App;
